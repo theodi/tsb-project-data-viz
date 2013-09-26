3e75dcddc43af24921d9ec1e33f4b93c9961f5b5
vorg <marcin.ignac@gmail.com>
2013-09-26 11:10:16 +0100

    List of unique regions

--- 06_regions/DataSet.js
+++ 06_regions/DataSet.js
@@ -1,13 +1,39 @@
-function DataSet() {
+var DataSet = (function() {
 
-}
-
-DataSet.fromArray = function(array) {
-  var ds = new DataSet();
-  ds.rows = array;
-  ds.columns = [];
-  for(var columnName in array[0]) {
-    ds.columns.push(columnName);
+  function extract(list, propertyName) {
+    return list.map(function(item) { return item[propertyName] || 'Unknown' });
   }
-  return ds;
-}
+
+  function unique(list) {
+    var values = {};
+    var result = [];
+    list.forEach(function(value) {
+      if (!values[value]) {
+        values[value] = true;
+        result.push(value);
+      }
+    });
+    return result;
+  }
+
+  function DataSet() {
+
+  }
+
+  DataSet.fromArray = function(array) {
+    var ds = new DataSet();
+    ds.rows = array;
+    ds.columns = [];
+    for(var columnName in array[0]) {
+      ds.columns.push(columnName);
+    }
+    return ds;
+  }
+
+  DataSet.prototype.uniqueValues = function(fieldName) {
+    return unique(extract(this.rows, fieldName));
+  }
+
+  return DataSet;
+
+})();
