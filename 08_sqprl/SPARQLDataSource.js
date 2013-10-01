89e65542fefd958f39465af8d1506e68cb4b862e
vorg <marcin.ignac@gmail.com>
2013-10-01 13:03:07 +0100

    Switched to DataSet and promisses

--- 08_sqprl/SPARQLDataSource.js
+++ 08_sqprl/SPARQLDataSource.js
@@ -4,34 +4,55 @@ function SPARQLDataSource() {
 
 SPARQLDataSource.endpoint = "proxy.php";
 
-SPARQLDataSource.prototype.query = function(q)
+SPARQLDataSource.prototype.query = function(queryStr) {
   var deferred = Q.defer();
+  var self = this;
 
   var sparqler = new SPARQL.Service(SPARQLDataSource.endpoint);
   sparqler.setPrefix("tsb", "http://tsb-projects.labs.theodi.org/def/");
   sparqler.setPrefix("rdf", "http://www.w3.org/2000/01/rdf-schema#");
   sparqler.setOutput("json");
   var query = sparqler.createQuery();
-  query.query(
-    " \
+  query.query(queryStr, {
+    failure: function(err) { deferred.reject(new Error(err)); },
+    success: function(json) {
+      var data = json.results.bindings.map(self.extractValues);
+      var dataSet = DataSet.fromArray(data);
+      deferred.resolve(dataSet);
+    }
+  });
+
+  return deferred.promise;
+}
+
+SPARQLDataSource.prototype.extractValues = function(binding) {
+  var o = {};
+  for(var propertyName in binding) {
+    o[propertyName] = binding[propertyName].value;
+  }
+  return o;
+}
+
+SPARQLDataSource.prototype.getProjects = function() {
+  var q =" \
     SELECT (?p as ?uri) ?label \
     WHERE { \
       ?p a tsb:Project . \
       ?p rdf:label ?label \
-    }",
-    {failure: onFailure, success: onSuccess}
-  );
+    }";
 
-  function onFailure(err) {
-    deferred.reject(new Error(err));
-  }
+  return this.query(q);
+}
 
-  function onSuccess(json) {
-    var projects = json.results.bindings.map(function(binding) {
-      return { uri : binding.uri.value, label : binding.label.value };
-    });
-    deferred.resolve(projects[0]);
-  }
+SPARQLDataSource.prototype.getProjectsParticipants = function() {
+  var q =" \
+    SELECT ?project ?projectLabel ?participant ?participantLabel \
+    WHERE { \
+      ?project a tsb:Project . \
+      ?project rdf:label ?projectLabel . \
+      ?project tsb:hasParticipant ?participant . \
+      ?participant rdf:label ?participantLabel . \
+    }";
 
-  return deferred.promise;
+  return this.query(q);
 }
