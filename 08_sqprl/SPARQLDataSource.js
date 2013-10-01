038908f5d986f4e80a6f55b1c921794d3f2640ec
vorg <marcin.ignac@gmail.com>
2013-10-01 12:39:52 +0100

    Switched to promises with q.js

--- 08_sqprl/SPARQLDataSource.js
+++ 08_sqprl/SPARQLDataSource.js
@@ -5,6 +5,8 @@ function SPARQLDataSource() {
 SPARQLDataSource.endpoint = "proxy.php";
 
 SPARQLDataSource.prototype.getProjects = function(callback) {
+  var deferred = Q.defer();
+
   var sparqler = new SPARQL.Service(SPARQLDataSource.endpoint);
   sparqler.setPrefix("tsb", "http://tsb-projects.labs.theodi.org/def/");
   sparqler.setPrefix("rdf", "http://www.w3.org/2000/01/rdf-schema#");
@@ -16,13 +18,15 @@ SPARQLDataSource.prototype.getProjects = function(callback) {
   );
 
   function onFailure(err) {
-    if (callback) callback(err);
+    deferred.reject(new Error(err));
   }
 
   function onSuccess(json) {
     var projects = json.results.bindings.map(function(binding) {
       return { url : binding.s.value };
-    })
-    if (callback) callback(projects);
+    });
+    deferred.resolve(projects);
   }
+
+  return deferred.promise;
 }
