a37b14707fe40ea713c2912a6be4bd4d144b987e
vorg <marcin.ignac@gmail.com>
2013-10-03 13:34:57 +0100

    Added command line arguments to capture.js

--- 07_svg_capture/capture.js
+++ 07_svg_capture/capture.js
@@ -1,13 +1,17 @@
 var page = require('webpage').create();
+var system = require('system');
 console.log('loading...');
 page.viewportSize = { width: 1280, height: 720 };
-page.open('http://theodi.github.io/tsb-project-data-viz/06_regions', function () {
+
+var url = (system.args.length > 1) ? system.args[1] : 'http://theodi.org'
+var destination = (system.args.length > 2) ? system.args[2] : 'screenshot.png';
+page.open(url, function () {
   console.log('loaded');
   setTimeout(function() {
     var title = page.evaluate(function() {
         return document.body.style.background = "#FFFFFF";
     });
-    page.render('05_grants_by_year.jpg');
+    page.render(destination);
     phantom.exit();
   }, 3000);
 });
