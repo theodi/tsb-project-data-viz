886423aaec3efe311800ac9e7e5a9e6fc239733e
vorg <marcin.ignac@gmail.com>
2013-10-16 01:46:26 +0100

    Growing projects

--- 10_projectIdent/js/ProjectIdent.js
+++ 10_projectIdent/js/ProjectIdent.js
@@ -4,12 +4,6 @@ var odiColors = [
   '#D60303', '#EF3AAB', '#E6007C', '#B13198'
 ];
 
-var odiColorsBest = [
-  '#2254F4', '#08DEF9',
-  '#0DBC37', '#67EF67', '#F9BC26', '#FF6700',
-  '#D60303', '#E6007C'
-];
-
 var regionNameMap = [
   ['North West England', 'North West'],
   ['North East England', 'North East'],
@@ -23,7 +17,7 @@ var w = window.innerWidth;
 var h = 480;
 
 function loadData() {
-  d3.csv('../data/TSB-data-public-sample.csv', function(error, rows) {
+  d3.csv('../data/TSB-data-public-2012.csv', function(error, rows) {
     //normalize region names
     rows.forEach(function(row) {
       regionNameMap.forEach(function(regionMapping) {
@@ -41,12 +35,18 @@ function loadData() {
       projectList.push(projects[projectId]);
     });
 
+    console.log('Loaded', projectList.length, 'projects');
+
     var budgetAreaToColor = (function() {
       var colorMap = {};
       var idx = 0;
       return function(area) {
+        if (area.trim() == 'TSB Programmes') area = 'TSBProgrammes';
         if (!colorMap[area]) {
-          colorMap[area] = odiColorsBest[idx++];
+          colorMap[area] = odiColors[idx++];
+          if (!colorMap[area]) {
+            colorMap[area] = '#DDD';
+          }
           console.log('new mapping', area, colorMap[area], idx);
         }
         return colorMap[area];
@@ -58,27 +58,29 @@ function loadData() {
         .append('g')
         .append('rect')
         .attr('class', className)
-        .attr('x', x).attr('y', y)
-        .attr('width', w).attr('height', h)
+        .attr('x', x).attr('y', y+h)
+        .attr('width', w).attr('height', 0)
         .attr('fill', color)
+        .transition().delay(Math.random()*50000)
+        .attr('y', y)
+        .attr('height', h);
     }
 
     projectList.forEach(function(project, projectIndex) {
       spacingX = 8
-      spacingY = 40
+      spacingY = 25
       margin = 40
       projectsPerLine = Math.floor((w - 2*margin) / spacingX)
       px = margin + (projectIndex % projectsPerLine) * spacingX
       py = margin + Math.floor(projectIndex / projectsPerLine) * spacingY
       pw = 5
-      ph = 20
+      ph = 15
       budgetArea = project.rows[0]['AreaBudgetHolder']
       var color = budgetAreaToColor(budgetArea);
-      for(var i=0; i<project.rows.length; i++) {
-        makeRect(px, py + 3*i, pw, 2, color, 'project');
-      }
-      //makeRect(px, py, pw, ph, color, 'project');
-      //makeRect(px, py + ph/2+1, pw, ph/2-2, color, 'project');
+      //for(var i=0; i<project.rows.length; i++) {
+      //  makeRect(px, py + 3*i, pw, 2, color, 'project');
+      //}
+      makeRect(px, py, pw, ph, color, 'project');
     })
   });
 }
