11d3cf695340cc8db7df4477e5ac25b7eabd289a
vorg <marcin.ignac@gmail.com>
2013-10-22 09:17:16 +0100

    Added hash scene switching

--- 13_bigviz/js/tsb.viz.main.js
+++ 13_bigviz/js/tsb.viz.main.js
@@ -14,10 +14,31 @@ function init() {
     .attr('width', tsb.state.w)
     .attr('height', tsb.state.h);
 
-  //tsb.viz.intro.init(svg, tsb.state.w, tsb.state.h);
-  //tsb.viz.priorityAreas.init(svg, tsb.state.w, tsb.state.h);
-  //tsb.viz.collaborations.init(svg, tsb.state.w, tsb.state.h);
-  tsb.viz.regions.init(svg, tsb.state.w, tsb.state.h);
+  window.addEventListener('hashchange', checkScene, false);
+  function checkScene() {
+    svg.remove();
+    svg = tsb.state.svg = d3.select('#home-viz').append('svg')
+    .attr('width', tsb.state.w)
+    .attr('height', tsb.state.h);
+    if (document.location.hash == '#intro') {
+      tsb.viz.intro.init(svg, tsb.state.w, tsb.state.h);
+    }
+    else if (document.location.hash == '#priorityareas') {
+      tsb.viz.priorityAreas.init(svg, tsb.state.w, tsb.state.h);
+    }
+    else if (document.location.hash == '#collaborations') {
+      tsb.viz.collaborations.init(svg, tsb.state.w, tsb.state.h);
+    }
+    else if (document.location.hash == '#regions') {
+      tsb.viz.regions.init(svg, tsb.state.w, tsb.state.h);
+    }
+    else {
+      tsb.viz.intro.init(svg, tsb.state.w, tsb.state.h);
+    }
+  }
+  checkScene();
 }
+
+
 
 window.onload = init;
