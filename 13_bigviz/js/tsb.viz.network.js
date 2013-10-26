989ee1d284832bb5b518568afae3c8f4afdfed31
vorg <marcin.ignac@gmail.com>
2013-10-27 00:22:41 +0100

    Force directed layout without links

--- 13_bigviz/js/tsb.viz.network.js
+++ 13_bigviz/js/tsb.viz.network.js
@@ -7,14 +7,130 @@ tsb.viz.network = {
     this.h = h;
     this.institutionSize = 'small';
     this.institutionTopCount = 12;
+
+    this.force = d3.layout.force()
+      .charge(-20)
+      .linkDistance(5)
+      .size([w, h]);
+
+    this.loadData();
+    this.resize(this.w, this.h);
+  },
+  organizationsByName: {},
+  organizations: [],
+  loadData: function() {
+    tsb.state.dataSource.getInstitutions(this.institutionSize).then(function(data) {
+      this.processRows(data.rows);
+
+      data.rows.sort(function(a, b) {
+        return b.numProjects - a.numProjects;
+      });
+
+      var organizationList = tsb.common.inital(data.rows, this.institutionTopCount);
+      var loadedCollabolators = 0;
+      organizationList.forEach(function(organization, organizationIndex) {
+        tsb.state.dataSource.getOrganizationCollaborators(organization.org).then(function(data) {
+          this.processRows(data.rows);
+          organization.collaborators = data.rows;
+          if (++loadedCollabolators == organizationList.length) {
+            this.buildViz(organizationList);
+          }
+        }.bind(this));
+      }.bind(this));
+    }.bind(this));
+  },
+  processRows: function(rows) {
+    rows.forEach(function(row) {
+      row.lat = Number(row.lat);
+      row.lng = Number(row.lng);
+      row.x = this.lngX(row.lng);
+      row.y = this.latY(row.lat);
+      if (row.numProjects) row.numProjects = Number(row.numProjects);
+      if (row.org) row.id = row.org.substr(row.org.lastIndexOf('/')+1)
+    }.bind(this));
+  },
+  buildViz: function(organizations) {
+    var organizationsNodes = [];
+    var organizationsLinks = [];
+    var organizationsByName = {};
+
+
+    organizations.forEach(function(organization) {
+      organization.index = organizationsNodes.length;
+      organizationsByName[organization.orgLabel] = organization;
+      organizationsNodes.push(organization);
+      organization.collaborators.forEach(function(collaborator) {
+        console.log(collaborator.collaboratorLabel);
+        if (organizationsByName[collaborator.collaboratorLabel]) {
+          collaborator = organizationsByName[collaborator.label];
+        }
+        else {
+          collaborator.index = organizationsNodes.length;
+          organizationsByName[collaborator.collaboratorLabel] = collaborator;
+          organizationsNodes.push(collaborator);
+        }
+      });
+    });
+
+    this.force
+      .nodes(organizationsNodes)
+      .links(organizationsLinks)
+      .start();
+
+    var organizationSite = this.svg.selectAll('circle.organization')
+      .data(organizationsNodes)
+        .enter()
+        .append('circle')
+        .attr('class', 'node')
+        .attr('cx', function(d) { return d.x; })
+        .attr('cy', function(d) { return d.y; })
+        .attr('r', 3)
+        .style('stroke', '#44FF00')
+        .style('fill', 'none');
+
+    this.force.on('tick', function() {
+      // link.attr('x1', function(d) { return d.source.x; })
+      //   .attr('y1', function(d) { return d.source.y; })
+      //   .attr('x2', function(d) { return d.target.x; })
+      //   .attr('y2', function(d) { return d.target.y; });
+
+      organizationSite.attr('cx', function(d) { return d.x; })
+        .attr('cy', function(d) { return d.y; });
+    });
+  },
+  resize: function(w, h) {
+    this.w = w;
+    this.h = h;
+    var dlng = 2.856445+5.458984;
+    var dlat = 56.291349-49.95122;
+    this.lngX = d3.scale.linear()
+      //.domain([-10.458984, 2.856445])
+      .domain([-5.458984, 1.3])
+      .range([(w-h)/2, h*dlat/dlng+(w-h)/2]);
+      //.range([0, w]);
+    this.latY = d3.scale.linear()
+      //.domain([61.291349, 49.95122])
+      .domain([57.291349, 49.95122])
+      .range([0, h]);
+  },
+}
+
+/*
+tsb.viz.network = {
+  init: function(svg, w, h) {
+    this.svg = svg;
+    this.w = w;
+    this.h = h;
+    this.institutionSize = 'small';
+    this.institutionTopCount = 12;
     this.loadData();
 
     this.resize(w, h);
 
     this.voronoi = d3.geom.voronoi()
       .clipExtent([[0, 0], [this.w, this.h]])
-      //.x(function(d) { return d.x; })
-      //.y(function(d) { return d.y; })
+      .x(function(d) { return d.x; })
+      .y(function(d) { return d.y; })
     this.path = this.svg.append('g').selectAll('path');
   },
   organizationList: [],
@@ -41,7 +157,8 @@ tsb.viz.network = {
       organization.x = this.lngX(organization.lng);
       organization.y = this.latY(organization.lat);
 
-      this.organizationList.push([organization.x, organization.y]);
+      organization.index = this.organizationList;
+      this.organizationList.push(organization);
 
       var x = this.w / 2;
       var y = this.h / 2;
@@ -62,19 +179,25 @@ tsb.viz.network = {
       }.bind(this));
     }.bind(this));
   },
+  organizationsByName: {},
+
   totalDots: 0,
   uniqueDots: {},
   uniqueDotsCount: 0,
+  loadedSets: 0,
   addCollaborators: function(organization, organizationGroup, radius, collaboratorsDataSet) {
+    this.loadedSets++;
     collaboratorsDataSet.rows.forEach(function(collaboratorInfo) {
       collaboratorInfo.lat = Number(collaboratorInfo.lat);
       collaboratorInfo.lng = Number(collaboratorInfo.lng);
-      collaboratorInfo.x = Math.random()*this.w;//this.lngX(collaboratorInfo.lng) + 15 * Math.random();
-      collaboratorInfo.y = Math.random()*this.h;//this.latY(collaboratorInfo.lat) + 15 * Math.random();
+      collaboratorInfo.x = this.lngX(collaboratorInfo.lng) + 15 * Math.random();
+      collaboratorInfo.y = this.latY(collaboratorInfo.lat) + 15 * Math.random();
+      //collaboratorInfo.x = Math.random()*this.w;
+      //collaboratorInfo.y = Math.random()*this.h;
       collaboratorInfo.budgetAreaCode = tsb.common.extractBudgetAreaCode(collaboratorInfo.budgetArea);
       if (!this.uniqueDots[collaboratorInfo.collaboratorLabel]) {
         this.uniqueDots[collaboratorInfo.collaboratorLabel] = true;
-        this.organizationList.push([collaboratorInfo.x, collaboratorInfo.y]);
+        this.organizationList.push(collaboratorInfo);
         this.uniqueDotsCount++;
       }
       else {
@@ -102,7 +225,9 @@ tsb.viz.network = {
           return '#666666'
         })
     }.bind(this));
-    this.updateMesh();
+    if (++this.loadedSets == this.institutionTopCount) {
+      //this.updateMesh();
+    }
   },
   resize: function(w, h) {
     this.w = w;
@@ -112,8 +237,8 @@ tsb.viz.network = {
     this.lngX = d3.scale.linear()
       //.domain([-10.458984, 2.856445])
       .domain([-5.458984, 1.3])
-      //.range([(w-h)/2, h*dlat/dlng+(w-h)/2]);
-      .range([0, w]);
+      .range([(w-h)/2, h*dlat/dlng+(w-h)/2]);
+      //.range([0, w]);
     this.latY = d3.scale.linear()
       //.domain([61.291349, 49.95122])
       .domain([57.291349, 49.95122])
@@ -134,3 +259,4 @@ tsb.viz.network = {
 
   }
 }
+*/
